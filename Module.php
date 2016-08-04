90f88c018dedad8ab8ebf01c5ea40eb0ae7bbe9e
Sachin Gaikwad <gaikwad411@gmail.com>
2016-08-04 07:11:04 +0530

    Console command support added to module init

--- Module.php
+++ Module.php
@@ -7,5 +7,10 @@ class Module extends \yii\base\Module
         parent::init();
 	// initialize the module with the configuration loaded from config.php
     	\Yii::configure($this, require(__DIR__ . '/config.php'));
+
+       // For commands
+       if (Yii::$app instanceof \yii\console\Application) {
+        $this->controllerNamespace = 'app\modules\users-module\commands';
+       }
     }
 }

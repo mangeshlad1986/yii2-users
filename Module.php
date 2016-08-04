5f244d1a1102661c446e3eff955b036188232e37
Sachin Gaikwad <gaikwad411@gmail.com>
2016-08-04 07:04:01 +0530

    Module config file and load config from file code added

--- Module.php
+++ Module.php
@@ -5,5 +5,7 @@ class Module extends \yii\base\Module
     public function init()
     {
         parent::init();
+	// initialize the module with the configuration loaded from config.php
+    	\Yii::configure($this, require(__DIR__ . '/config.php'));
     }
 }

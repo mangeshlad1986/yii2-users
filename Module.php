d30e78cf12d60b85cf2acb60d1bb6b193324cf0e
Sachin Gaikwad <gaikwad411@gmail.com>
2016-08-10 22:23:53 +0530

    namespace changed

--- Module.php
+++ Module.php
@@ -1,16 +1,17 @@
-namespace yii2-users;
+<?php
+namespace tamarind;
 
 class Module extends \yii\base\Module
 {
     public function init()
     {
         parent::init();
-	// initialize the module with the configuration loaded from config.php
+	       // initialize the module with the configuration loaded from config.php
     	\Yii::configure($this, require(__DIR__ . '/config.php'));
 
        // For commands
        if (Yii::$app instanceof \yii\console\Application) {
-        $this->controllerNamespace = 'yii2-users\commands';
+        $this->controllerNamespace = 'tamarind\commands';
        }
     }
 }
--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -1,8 +1,12 @@
-namespace yii2-users\controllers;
+<?php
+namespace namespace tamarind\controllers;
 
 use yii\web\Controller;
 
 class DefaultController extends Controller
 {
     // ...
+    public function actionIndex(){
+		echo 'Test';
+    }
 }
